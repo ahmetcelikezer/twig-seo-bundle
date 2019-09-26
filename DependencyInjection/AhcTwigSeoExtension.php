876f3058dce6c2e6cbebcda1d3295b83c00aeba6
Ahmet <ahmet@atolye15.com>
2019-09-26 17:41:00 +0300

    Default Configuration Overwrite bug Solved

--- DependencyInjection/AhcTwigSeoExtension.php
+++ DependencyInjection/AhcTwigSeoExtension.php
@@ -12,6 +12,11 @@ use Symfony\Component\Yaml\Yaml;
 
 class AhcTwigSeoExtension extends Extension implements PrependExtensionInterface
 {
+    /**
+     * @var array
+     */
+    private $preLoadedConfigs;
+
     public function load(array $configs, ContainerBuilder $container)
     {
         $config = $this->processConfiguration(new Configuration(), $configs);
@@ -19,13 +24,29 @@ class AhcTwigSeoExtension extends Extension implements PrependExtensionInterface
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
 
+        $config = $this->overwriteDefaultConfigs($config);
+        dd($config);
         $container->setParameter('ahc_twig_seo_config', $config);
     }
 
     public function prepend(ContainerBuilder $container)
     {
-        $config = Yaml::parseFile(__DIR__.'/../Resources/config/ahc_twig_seo.yaml');
+        $configs = Yaml::parseFile(__DIR__.'/../Resources/config/ahc_twig_seo.yaml');
+        $this->preLoadedConfigs = $configs['ahc_twig_seo'];
+    }
 
-        $container->prependExtensionConfig('ahc_twig_seo', $config['ahc_twig_seo']);
+    private function overwriteDefaultConfigs(array $configs): array
+    {
+        $defaultGroups = array_keys($this->preLoadedConfigs['groups']);
+        $customGroups = array_keys($configs['groups']);
+
+        foreach ($customGroups as $customGroup) {
+            if(in_array($customGroup, $defaultGroups)) {
+                unset($this->preLoadedConfigs['groups'][$customGroup]);
+            }
+        }
+
+        return array_merge($this->preLoadedConfigs['groups'], $configs['groups']);
+
     }
 }
